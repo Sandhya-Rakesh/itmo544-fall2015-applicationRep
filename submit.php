c15196c3a90c89e67ebadcd6fd08c21b68095cea
Sandhya Gupta <sandhyaguptamp@gmail.com>
2015-10-25 13:31:34 -0500

    Added S3 resources

--- submit.php
+++ submit.php
@@ -1,5 +1,43 @@
-<html>
-  <body>
-    <h3>Successful</h3>
-  </body
-</html>
+<?php
+  // Start the session
+  session_start();
+  require 'vendor/autoload.php';
+  // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
+  // of $_FILES.
+  echo $_POST['useremail'];
+  $uploaddir = '/tmp/';
+  $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+  echo '<pre>';
+  if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+      echo "File is valid, and was successfully uploaded.\n";
+  } else {
+      echo "Possible file upload attack!\n";
+  }
+  echo 'Here is some more debugging info:';
+  print_r($_FILES);
+  print "</pre>";
+
+  #use Aws\S3\S3Client;
+  $s3 = new Aws\S3\S3Client([
+      'version' => 'latest',
+      'region'  => 'us-west-2'
+  ]);
+  $s3->createBucket(array('Bucket' => 'sandbucket'));
+  # AWS PHP SDK version 3 create bucket
+  $result = $s3->createBucket([
+      'ACL' => 'public-read',
+      'Bucket' => $bucket
+  ]);
+  $s3->waitUntil('BucketExists', array('Bucket' => $bucket));
+
+  # PHP version 3
+  $result = $s3->putObject([
+      'ACL' => 'public-read',
+      'Bucket' => $bucket,
+      'Key' => $uploadfile
+  ]);  
+
+  $url = $result['ObjectURL'];
+  echo $url;
+
+?>
